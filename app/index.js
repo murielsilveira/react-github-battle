0c1223a659824ed7f6a2112668b61b76de938788
Muriel Silveira <murielkong@gmail.com>
2016-03-20 20:10:17 -0300

    github profile

--- app/index.js
+++ app/index.js
@@ -1,14 +1,50 @@
+var USER = {
+    name: 'Muriel Silveira',
+    username: 'murielsilveira',
+    image: 'https://avatars0.githubusercontent.com/u/4459232?v=3&s=460'
+}
+
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var Hello = React.createClass({
-    render: function () {
+var ProfilePic = React.createClass({
+    render: function() {
+        return <img src={this.props.imageUrl} style={{heigth: 100, width: 100}}/>
+    }
+})
+
+var ProfileLink = React.createClass({
+    render: function() {
         return (
-            <div>Hello, {this.props.name} :)</div>
+            <div>
+                <a href={'https://github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        )
+    }
+})
+
+var ProfileName = React.createClass({
+    render: function() {
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
+})
+
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image}/>
+                <ProfileName name={this.props.user.name}/>
+                <ProfileLink username={this.props.user.username}/>
+            </div>
         )
     }
 })
 
 ReactDOM.render(
-    <Hello name="Muriel" />,
+    <Avatar user={USER}/>,
     document.getElementById('app'))
