d34984d084585955bfef570a1e83b322ca9567d7
Muriel Silveira <murielkong@gmail.com>
2016-03-21 00:11:27 -0300

    clean up

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,11 +3,12 @@ var ReactRouter = require('react-router')
 var Router = ReactRouter.Router
 var Route = ReactRouter.Route
 var IndexRoute = ReactRouter.IndexRoute
+var hashHistory = ReactRouter.hashHistory
 var Main = require('../components/Main')
 var Home = require('../components/Home')
 
 var routes = (
-    <Router>
+    <Router history={hashHistory}>
         <Route path='/' component={Main}>
             <IndexRoute component={Home}/>
         </Route>
